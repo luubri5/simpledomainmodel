ec493bd1cb1509c95dcadf2e01c55917a6d93263
Brian Luu <luubri5@uw.edu>
2019-01-22 22:20:49 -0800

    Tests passed

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -5,108 +5,156 @@
 //  Created by Ted Neward on 4/6/16.
 //  Copyright © 2016 Ted Neward. All rights reserved.
 //
-
 import Foundation
 
 print("Hello, World!")
 
 public func testMe() -> String {
-  return "I have been tested"
+    return "I have been tested"
 }
 
 open class TestMe {
-  open func Please() -> String {
-    return "I have been tested"
-  }
+    open func Please() -> String {
+        return "I have been tested"
+    }
 }
 
 ////////////////////////////////////
 // Money
-//
 public struct Money {
-  public var amount : Int
-  public var currency : String
-  
-  public func convert(_ to: String) -> Money {
-  }
-  
-  public func add(_ to: Money) -> Money {
-  }
-  public func subtract(_ from: Money) -> Money {
-  }
+    public var amount : Int
+    public var currency : String
+    private var currencies = ["USD": 1.0, "GBP": 0.5, "EUR" : 1.5, "CAN" : 1.25]
+    init(amount: Int, currency : String) {
+        if  !currencies.keys.contains(currency) {
+            print("Unaccepted")
+            self.amount = 0
+            self.currency = ""
+        } else {
+            self.amount = amount
+            self.currency = currency
+        }
+    }
+   
+    public func convert(_ to: String) -> Money {
+        return Money(amount: Int(Double(self.amount) / self.currencies[self.currency]! * self.currencies[to]!), currency: to)
+    }
+    
+    public func add(_ to: Money) -> Money {
+        if to.currency != self.currency {
+            let converted : Money = convert(to.currency)
+            return Money(amount: converted.amount + to.amount, currency: to.currency)
+        }
+        return Money(amount: self.amount + to.amount, currency: to.currency)
+    }
+    
+    public func subtract(_ from: Money) -> Money {
+        var curMoney = from
+        if from.currency != self.currency {
+            curMoney = self.convert(from.currency)
+        }
+        return Money(amount: from.amount - curMoney.amount, currency: from.currency)
+    }
 }
 
 ////////////////////////////////////
 // Job
-//
 open class Job {
-  fileprivate var title : String
-  fileprivate var type : JobType
-
-  public enum JobType {
-    case Hourly(Double)
-    case Salary(Int)
-  }
-  
-  public init(title : String, type : JobType) {
-  }
-  
-  open func calculateIncome(_ hours: Int) -> Int {
-  }
-  
-  open func raise(_ amt : Double) {
-  }
+    fileprivate var title : String
+    fileprivate var type : JobType
+    
+    public enum JobType {
+        case Hourly(Double)
+        case Salary(Int)
+    }
+    
+    
+    public init(title : String, type : JobType) {
+        self.title = title
+        self.type = type
+    }
+    
+    open func calculateIncome(_ hours: Int) -> Int {
+        switch self.type{
+            case.Salary(let salary): return Int(salary)
+            case.Hourly(let hourly): return Int(Double(hours) * hourly)
+        }
+    }
+    
+    open func raise(_ amt : Double) {
+        switch self.type{
+            case .Salary(let salary): self.type = JobType.Salary(salary+Int(amt))
+            case .Hourly(let hourly): self.type = JobType.Hourly(hourly + amt)
+        }
+    }
 }
 
 ////////////////////////////////////
 // Person
-//
 open class Person {
-  open var firstName : String = ""
-  open var lastName : String = ""
-  open var age : Int = 0
+    open var firstName : String = ""
+    open var lastName : String = ""
+    open var age : Int = 0
+    
+    fileprivate var _job : Job? = nil
+    open var job : Job? {
+        get { return _job }
+        set(value) {
+            if age > 16 {
+                _job = value
+            }
+        }
+    }
+    
+    fileprivate var _spouse : Person? = nil
+    open var spouse : Person? {
+        get { return _spouse }
+        set(value) {
+            if (age > 18) {
+                _spouse = value
+            }
+        }
+    }
+    
+    public init(firstName : String, lastName: String, age : Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+    
+    open func toString() -> String {
+        return "[Person: firstName:\(firstName) lastName:\(lastName) age:\(age) job:\(String(describing: job)) spouse:\(String(describing: spouse))]"
 
-  fileprivate var _job : Job? = nil
-  open var job : Job? {
-    get { }
-    set(value) {
     }
-  }
-  
-  fileprivate var _spouse : Person? = nil
-  open var spouse : Person? {
-    get { }
-    set(value) {
-    }
-  }
-  
-  public init(firstName : String, lastName: String, age : Int) {
-    self.firstName = firstName
-    self.lastName = lastName
-    self.age = age
-  }
-  
-  open func toString() -> String {
-  }
 }
 
 ////////////////////////////////////
 // Family
-//
 open class Family {
-  fileprivate var members : [Person] = []
-  
-  public init(spouse1: Person, spouse2: Person) {
-  }
-  
-  open func haveChild(_ child: Person) -> Bool {
-  }
-  
-  open func householdIncome() -> Int {
-  }
+    fileprivate var members : [Person] = []
+    
+    public init(spouse1: Person, spouse2: Person) {
+        spouse1.spouse = nil
+        spouse2.spouse = nil
+        spouse1.spouse = spouse2
+        spouse2.spouse = spouse1
+        members = [spouse1, spouse2]
+        
+    }
+    
+    open func haveChild(_ child: Person) -> Bool {
+        if members[0].age > 21 && members[1].age > 21 {
+            members.append(child)
+            return true
+        }
+        return false
+    }
+    
+    open func householdIncome() -> Int {
+        var sum = 0
+        for mem in self.members{
+            sum += mem.job?.calculateIncome(2000) ?? 0
+        }
+        return sum
+    }
 }
-
-
-
-
-
